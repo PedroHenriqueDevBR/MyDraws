db43c16f323ecc944651bafe30aa2df4cfd85c73
PedroHenriqueDevBR <p.lyma18@gmail.com>
2025-07-23 04:21:01 -0300

    chore: change ai prompt

--- core/services/design_by_ai.py
+++ core/services/design_by_ai.py
@@ -20,102 +20,16 @@ class DesignByAI:
         self.client = genai.Client(api_key=config("GENAI_API_KEY"))  # type: ignore
         self.text_input = (
             "Convert this image into a black and white line drawing"
-            "in coloring book style. Preserve the key shapes and "
-            "features of the original image — whether it's a person, "
+            "in coloring book style. — whether it's a person, "
             "object, animal, or landscape — using only clean and "
             "sharp outlines, with no shading or color fill. The "
-            "result should clearly reflect the identity and "
-            "structure of the image, in a style suitable for "
-            "printing and hand coloring with pencils or markers. "
-            "Design the background to be minimalistic - "
+            "result should cartoonishly reflect the identity and "
+            "structure of the image's main elements, in a simple "
+            "style suitable for printing and hand coloring with "
+            "pencils or markers. Design the background to be minimalistic too."
             "Don't forget the hair or beard design when it's a person"
         )
-        # self.text_input = (
-        #     "Convert this image into a black and white line drawing"
-        #     "in coloring book style. — whether it's a person, "
-        #     "object, animal, or landscape — using only clean and "
-        #     "sharp outlines, with no shading or color fill. The "
-        #     "result should cartoonishly reflect the identity and "
-        #     "structure of the image's main elements, in a simple "
-        #     "style suitable for printing and hand coloring with "
-        #     "pencils or markers. Design the background to be minimalistic too."
-        # )
         self.model = model_name
-
-    def generate_from_model(
-        self,
-        image_model: str = "imagen-3.0-capability-001",
-    ) -> list[tuple[str, Image.Image]] | None:
-        """
-        imagen-3.0-capability-001
-        models/imagen-3.0-generate-002
-        models/imagen-4.0-generate-preview-06-06
-        """
-
-        token = (
-            subprocess.check_output("gcloud auth print-access-token", shell=True)
-            .decode()
-            .strip()
-        )
-        project = (
-            subprocess.check_output(
-                "gcloud config list --format='value(core.project)'", shell=True
-            )
-            .decode()
-            .strip()
-        )
-
-        headers = {
-            "Content-Type": "application/json",
-            "X-Goog-User-Project": project,
-            "Authorization": f"Bearer {token}",
-        }
-
-        response = self.client.models.edit_image(
-            model=image_model,
-            prompt=self.text_input,
-            reference_images=[
-                types.RawReferenceImageOrDict(  # type: ignore
-                    reference_image=self.image,
-                    reference_id=1,
-                )
-            ],
-            config=types.EditImageConfigOrDict(  # type: ignore
-                number_of_images=1,
-                output_mime_type="image/jpeg",
-                person_generation="ALLOW_ADULT",
-                http_options=types.HttpOptionsOrDict(  # type: ignore
-                    headers=headers,
-                ),
-            ),
-        )
-
-        if not response.generated_images:
-            print("No images generated.")
-            return
-
-        base_filename = os.path.basename(self.image_filename)
-        name_no_ext = os.path.splitext(base_filename)[0]
-
-        converted_dir = os.path.join(settings.MEDIA_ROOT, "temp")
-        if not os.path.exists(converted_dir):
-            os.makedirs(converted_dir)
-
-        if len(response.generated_images) != 1:
-            print("Number of images generated does not match the requested number.")
-
-        result = []
-
-        for index, generated_image in enumerate(response.generated_images):
-            ia_filename = name_no_ext + f"_ia_{index}.jpg"
-            ia_path = os.path.join(converted_dir, ia_filename)
-            image = Image.open(
-                BytesIO(generated_image.image.image_bytes),  # type: ignore
-            )
-            image.save(ia_path)
-            result.append((ia_path, image))
-
-        return result
 
     def generate_from_gemini(self) -> tuple[str, Image.Image]:
         response = self.client.models.generate_content(
